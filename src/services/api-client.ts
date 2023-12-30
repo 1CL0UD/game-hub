82d374624d8bb1926ad9c9273c8d71dfa3589d12
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2023-12-30 21:57:16 +0700

    9 - Note - API Key on .env file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '970ece1313824d0492b047841c35bf64',
+    key: import.meta.env.VITE_RAWG_KEY,
   },
 });
