6748a27f4ebf57e2c580fabd96e8c6df03c5c07a
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2023-12-31 09:38:37 +0700

    19 - Displaying Genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: '1fr',
+        lg: '16rem 1fr',
+      }}
     >
       <GridItem area={'nav'}>
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area={'aside'}>
+        <GridItem area={'aside'} paddingX={'1rem'}>
           <GenreList />
         </GridItem>
       </Show>
