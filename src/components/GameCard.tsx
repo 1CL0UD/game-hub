deb95e4afb3c110cea831d24c92ea00d249a2b68
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2024-01-02 23:37:52 +0700

    34 - Clean Up the Gaem Cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,13 +12,13 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={10} overflow={'hidden'}>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize={'2xl'}>{game.name}</Heading>
-        <HStack justify={'space-between'}>
+        <HStack justify={'space-between'} marginBottom={2}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize={'2xl'}>{game.name}</Heading>
       </CardBody>
     </Card>
   );
